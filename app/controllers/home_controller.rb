d6da87e86cbb1cf528676d8a27b05ec4cf36a559
Kathy Koubek <kkoubek@frontier.com>
2017-01-27 23:48:26 +0000

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  def index
+    @current_time = Time.now
+  end
+
+  def about
+  end
+
+  def contact
+  end
+
+  def faq
+  end
+  
+  def orion_nebula
+  end
+  
+  def andromeda
+  end
+  
+end
